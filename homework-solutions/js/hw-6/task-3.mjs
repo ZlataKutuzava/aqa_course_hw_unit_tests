2fddc20154e9065813d9efb85da248731cbfd771
Zlata <lottie.kett@gmail.com>
2025-08-07 21:06:13 +0300

    Add howetask for Homework 6

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,15 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+let unique = [];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+
+ for (let element of array) {
+  if (!unique.includes(element)) {
+    unique.push(element);
+  }
+ }
+
+ console.log(unique);
 
 export { unique };
